e072d8352fb9f95233bad46e7715257b7e067d17
Tristan Jet <tristanjetlay@gmail.com>
2023-12-27 00:09:13 +0200

    refactored

--- src/server.js
+++ src/server.js
@@ -2,15 +2,40 @@ const http = require('http')
 const WebSocket = require('ws')
 
 const app = require('./app');
-const onUpgrade = require('./websocket')
+const upgradeAuth = require('./websocket')
 
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ noServer: true });
 
-server.on('upgrade', (request, socket) => {
-  onUpgrade(request, socket)
+wss.on('connection', (ws, request) => {
+  console.log(`This token is good: ${request.headers.cookie}`);
+  console.log('Connection gucci')
+  ws.close()
+})
+
+server.on('upgrade', (request, socket, head) => {
+  const pathname = request.url;
+  if (pathname === '/ws') {
+    upgradeAuth(request)
+      .then(authed => {
+        if (authed) {
+          wss.handleUpgrade(request, socket, head, (ws) => {
+            wss.emit('connection', ws, request);
+          });
+        } else {
+          console.log('Unauthorized connection attempted')
+          socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+          socket.destroy();
+        }
+      })
+  } else {
+    console.log('Unauthorized connection attempted')
+    socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+    socket.destroy();
+  }
 });
+
 
 server.listen(port, () => {
   /* eslint-disable no-console */
--- src/websocket/index.js
+++ src/websocket/index.js
@@ -2,33 +2,14 @@ const cookie = require('cookie')
 
 const { sessionExists } = require("../repository");
 
-const onUpgrade = async (request, socket) => {
-  const pathname = request.url;
-
-  if (pathname === '/ws') {
-
-    await upgradeAuth(request, socket)
-    /*wss.handleUpgrade(request, socket, head, (ws) => {
-      wss.emit('connection', ws, request);
-    });*/
-  } else {
-    socket.destroy();
-  }
-}
-
-const upgradeAuth = async (request, socket) => {
-  console.log('here auth')
+const upgradeAuth = async (request) => {
   const parsedCookies = cookie.parse(request.headers.cookie || "");
   const sessionToken = parsedCookies["jet-session"];
 
   if (!sessionToken || !(await sessionExists(sessionToken))) {
-    socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
-    socket.destroy();
-    return;
+    return false;
   }
-
-  console.log(`This token is good: ${request.headers.cookie}`);
-  socket.destroy();
+  return true
 };
 
-module.exports = onUpgrade
+module.exports = upgradeAuth
