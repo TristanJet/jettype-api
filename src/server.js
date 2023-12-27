d825e21312f0f29fe2e782859b836bd53c32e97c
Tristan Jet <tristanjetlay@gmail.com>
2023-12-27 02:11:38 +0200

    Error callbacks

--- src/server.js
+++ src/server.js
@@ -11,31 +11,42 @@ const wss = new WebSocket.Server({ noServer: true });
 wss.on('connection', (ws, request) => {
   console.log(`This token is good: ${request.headers.cookie}`);
   console.log('Connection gucci')
-  ws.close()
+
+  ws.on('error', (err) => {
+    socket.destroy()
+    console.log(`Websocket error: ${err}`)
+  })
+
+  ws.on('message', onMessage)
+
 })
 
 server.on('upgrade', (request, socket, head) => {
-  const pathname = request.url;
-  if (pathname === '/ws') {
-    upgradeAuth(request)
-      .then(authed => {
-        if (authed) {
-          wss.handleUpgrade(request, socket, head, (ws) => {
-            wss.emit('connection', ws, request);
-          });
-        } else {
-          console.log('Unauthorized connection attempted')
-          socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
-          socket.destroy();
-        }
-      })
-  } else {
-    console.log('Unauthorized connection attempted')
-    socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
-    socket.destroy();
+  try {
+    const pathname = request.url;
+    if (pathname === '/ws') {
+      upgradeAuth(request)
+        .then(authed => {
+          if (authed) {
+            wss.handleUpgrade(request, socket, head, (ws) => {
+              wss.emit('connection', ws, request);
+            });
+          } else {
+            console.log('Unauthorized connection attempted')
+            socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+            socket.destroy();
+          }
+        })
+    } else {
+      console.log('Unauthorized connection attempted')
+      socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+      socket.destroy();
+    }
+  } catch (e) {
+    socket.destroy()
+    console.log('On connect error')
   }
 });
-
 
 server.listen(port, () => {
   /* eslint-disable no-console */
