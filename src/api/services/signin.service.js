184dd77250a710fefd1c4bd63bbea8b6e19f10e6
Tristan Jet <tristanjetlay1@gmail.com>
2024-06-03 18:55:10 +0300

    now i got the bug i think

--- src/api/services/signin.service.js
+++ src/api/services/signin.service.js
@@ -12,29 +12,27 @@ const createUserAndSession = async (jwt) => {
   }
   const sessionId = createId();
   await createSession(decoded.sub, sessionId, 'signed');
-  return sessionId;
+  return [decoded.sub, sessionId];
 };
 
 const handleSignin = async (req) => {
   if (!req.cookies['jet-session']) {
-    return await createUserAndSession(req.body.credential);
+    const [userId, sessionId] = await createUserAndSession(req.body.credential);
+    return sessionId;
   }
   if (!(await sessionExists(req.cookies['jet-session']))) {
-    return await createUserAndSession(req.body.credential);
+    const [userId, sessionId] = await createUserAndSession(req.body.credential);
+    return sessionId;
   }
   if (await getAuthTypeFromSession(req.cookies['jet-session']) === 'guest') {
     let guestUserId = await getUserIdFromSession(req.cookies['jet-session']);
     if (await userExists(guestUserId)) {
       const [name, avgWPM, totalCrowns] = await getUserData(guestUserId);
-      const signedUserId = req.body.credential;
-      const sessionId = await createUserAndSession(signedUserId);
+      const [signedUserId, sessionId] = await createUserAndSession(req.body.credential);
       await setUserData(signedUserId, name, avgWPM, totalCrowns);
       return sessionId
     }
   }
-
-  return await createUserAndSession(req.body.credential);
-
 }
 
 module.exports = handleSignin;
